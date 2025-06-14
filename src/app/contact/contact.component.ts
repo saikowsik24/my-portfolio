f4325d2b2ee1afe1263e51bd603a1d8489aeab0f
Meda Kowsik <saikowsik24@gmail.com>
2025-06-14 15:03:45 +0530

    corrected few sections and testing the another style

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -21,7 +21,7 @@ export class ContactComponent {
   };
 
   formSubmitted = false;
-  Email : string ="pdkv1999@gmail.com"
+  Email : string ="saikowsik24@gmail.com"
   constructor(private http: HttpClient) {}
 
   onSubmit() {
